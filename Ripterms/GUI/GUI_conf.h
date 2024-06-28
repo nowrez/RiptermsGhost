bbb0ac159d948eaaa271fc8d44ce2eef83f48805
nowrez <113263830+nowrez@users.noreply.github.com>
2024-06-29 02:52:05 +0500

    Update GUI_conf.h

--- Ripterms/GUI/GUI_conf.h
+++ Ripterms/GUI/GUI_conf.h
@@ -3,7 +3,7 @@
 
 namespace Ripterms::GUI
 {
-	const char* client_name = "r i p t e r m s    g h o s t";
+	const char* client_name = "r e a d d o n e    3 1 3";
 	constexpr float HEADER_HEIGHT = 40.f;
 	constexpr float HEADER_CAT_DISTANCE = 10.f;
 	constexpr float CATEGORY_WIDTH = 100.0f;
